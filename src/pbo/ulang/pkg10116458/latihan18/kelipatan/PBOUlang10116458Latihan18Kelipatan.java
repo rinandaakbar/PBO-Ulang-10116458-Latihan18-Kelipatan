2ba50b610e251c0ba1898b1c1a13af650a18f296
nanda <nanda@nanda>
2019-10-07 10:54:42 +0700

    commit men

---
+++ src/pbo/ulang/pkg10116458/latihan18/kelipatan/PBOUlang10116458Latihan18Kelipatan.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo.ulang.pkg10116458.latihan18.kelipatan;
+
+/**
+ *
+ * @author
+ * NAMA     : Rinanda Al Qorrie Akbar
+ * NIM      : 10116458
+ */
+public class PBOUlang10116458Latihan18Kelipatan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        double i=3.5;
+        do{
+            if (i >= 3.5) {
+                System.out.println(i + "");
+            }
+             i +=3.5;
+        }
+        while (i <=35); {
+         
+    }
+    }
+    
+}
